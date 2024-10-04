010bd0c7ca140459b42e737c22e10310e6b38c4e
Gabriel <gmonteiro810@gmail.com>
2024-10-04 15:44:10 -0300

    done

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,25 +1,32 @@
-﻿using ByteBankIO;
+﻿using System.Text;
 
-class Program
+
+//1 PASSO: Definir o endereço do aruivo
+var FileAddress = "C:\\Csharp\\Csharp_TrabalhandoComArquivos\\ByteBankIO\\contas.txt";
+var bytesRead = -1; 
+
+//2 PASSO: Criar um FileStream e passar qual operação será utilizada
+//o fileStream le o arquivo parte por parte sem ler o mesmo inteiro
+var fileStream = new FileStream(FileAddress, FileMode.Open);
+
+//3: Criar um buffer para armazenar os bytes lidos do arquivo
+var buffer = new byte[1024];
+
+//4: Enquanto o arquivo não terminar de ser lido
+while (bytesRead != 0)
 {
-    static void Main(string[] args)
-    {
+    //5: Passar o buffer para o FileStream e ler o arquivo onde ele vai ler a partir da posição 0 até 1024 bytes lendo item por item
+     bytesRead = fileStream.Read(buffer, 0, 1024);
+     WriteBuffer(buffer);
 
-        //1: Localizar o arquivo
-        var enderecoDoArquivo = "C:\\Users\\gmont\\OneDrive\\Desktop\\Csharp_TrabalhandoComArquivos\\ByteBankIO\\contas.txt";
-
-        //2: Criar um fluxo de bytes para ler o arquivo
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        //                                  local                   método que será utilizado
-
-        //3: Recuperar os bytes do arquivo
-        var buffer = new byte[1024];
-
-        //4: 1.parametro:Array de Bytes que já foram lidos
-        //2.parametro: local de inicio
-        //3.parametro: quantas posições serão preenchidas
-        fluxoDoArquivo.Read(buffer);
-
-
-    }
 }
+
+//6: Criar um método para escrever o buffer, onde ele vai converter os bytes em string
+static void WriteBuffer(byte[] buffer)
+{
+    var utf8 = new UTF8Encoding();
+    var text = utf8.GetString(buffer);
+    Console.WriteLine(text);
+
+}
+
