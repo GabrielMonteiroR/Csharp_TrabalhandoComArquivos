a0f06269f8386319b868eadea947c56e55fefacb
Gabriel <gmonteiro810@gmail.com>
2024-10-08 14:19:51 -0300

    ...

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -2,25 +2,6 @@
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "C:\\Users\\gmont\\OneDrive\\Desktop\\Csharp_TrabalhandoComArquivos\\ByteBankIO\\contas.txt";
-
-        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-        {
-            var leitor = new StreamReader(fluxoDeArquivo);
-
-            // var linha = leitor.ReadLine();  le a primeira linha
-            // var text = leitor.ReadToEnd(); le o arquivo todo (de uma vez sem ser parte por parte)
-            // var numero = leitor.Read(); le o primeiro byte
-
-            //Enquanto o fluxo não chegar ao fim
-            while (!leitor.EndOfStream)
-            {
-                var linha = leitor.ReadLine();
-                System.Console.WriteLine(linha);
-            }
-
-
-            Console.WriteLine();
-        }
+       TestaEscrita();
     }
 }
